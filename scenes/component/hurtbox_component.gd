b41279b0fbb301cb1dc11f033f4b2ed9cac3facb
Enes <102901204+mec-cs@users.noreply.github.com>
2023-09-19 02:18:20 +0300

    hit animation & damage text & more upgrade opt

--- scenes/component/hurtbox_component.gd
+++ scenes/component/hurtbox_component.gd
@@ -3,11 +3,14 @@ class_name HurtboxComponent
 
 @export var health_component: Node
 
+# file location changes affects !! (manually)
+var floating_text_scene = preload("res://scenes/UI/floating_text.tscn")
+
 
 func _ready():
 	area_entered.connect(on_area_entered)
-	
-	
+
+
 func on_area_entered(other_area: Area2D):
 	if not other_area is HitboxComponent:
 		return
@@ -17,3 +20,13 @@ func on_area_entered(other_area: Area2D):
 		
 	var hitbox_component = other_area as HitboxComponent
 	health_component.damage(hitbox_component.damage)
+	
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+	
+	floating_text.global_position = global_position + (Vector2.UP * 16) # global pos of the hurtbox component + 16pix
+	
+	var format_string = "%0.1f"
+	if round(hitbox_component.damage) == hitbox_component.damage:
+		format_string = "%0.0f"
+	floating_text.start(format_string % hitbox_component.damage)
